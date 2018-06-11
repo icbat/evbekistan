3efe549bf7b768cb28d6e98139829300af70d4f4
Evan Cobb <cobb.evan@gmail.com>
2018-06-11 00:37:00 -0500

    updating build script to also copy modpack so there's not duplicates of that

--- build.bat
+++ build.bat
@@ -1,11 +1,16 @@
-@echo " -- Building Client"
+@echo " -- Starting Building Client"
 @echo "Deleting old archive"
 rm client/client.zip
 @echo "Copying mods into client"
 robocopy mods client/mods
+@echo "Copying forge into client"
+robocopy "bin/" client/bin
 cd client
 @echo "Zipping it"
 7z a -tzip client.zip bin mods
-@echo "Cleaning up client/mods"
+@echo "Cleaning up client"
 rm -rf mods
+rm -rf bin
 cd ..
+robocopy "bin/forge-1.12.2-14.23.4.2707-universal.jar" client/bin/modpack.jar
+@echo " -- Finished Building Client"
